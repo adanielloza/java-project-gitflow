e9b3b45b115aae24163a4d9379a1e5455d005bb0
adanielloza <123408012+adanielloza@users.noreply.github.com>
2025-01-13 14:28:08 +0000

    saludo-multilingue

--- Main.java
+++ Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("***************");
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
     }
 }
